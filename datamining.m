01105cbf7c39789dcd643ace28c372e5778d79a6
Adrian <adde.a@hotmail.com>
2019-11-26 09:44:52 +0100

    Added an ignore

--- datamining.m
+++ datamining.m
@@ -1,3 +1,4 @@
-%%
 %Authors: Adrian Andersson, Daniel Sahlin, Joel Paulsson
-%TNA009: Data Mining
+%TNA009: Computational Methods for Science and Engineering
+%Description: Data Mining Project using LGK and K-Means clustering
+
