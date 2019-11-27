6a4c081c7bf11ba92690ed3f96d55688e5e98c01
Adrian <adde.a@hotmail.com>
2019-11-27 16:12:23 +0100

    Changed Q to P and added some comments on K-Means

--- CosineDist.m
+++ CosineDist.m
@@ -1,5 +1,6 @@
 function [columns] = CosineDist(query, A)
+    %Calculate the cosine similarity between a column vector q and several,
+    %or one, columns vectors A
     columns = (query'*full(A))./(vecnorm(full(A)) .* norm(query));
-    %columns = sparse(columns);
 end
 
--- datamining.m
+++ datamining.m
@@ -11,9 +11,12 @@ load 'data/text-mining-medline_stemmed.mat';
 %% Query Matching
 clc
 
+%Compute distances between query and documents
 tol = 0.19;
-Match = CosineDist(q(:,9), A) > tol;
-Docs = find(Match > 0);
+Match = CosineDist(q(:,9), A);
+
+%Find the 3 closest documents
+[~, Docs] = maxk(Match, 3);
 %% K-Means clustering
 %Should K be equal to the number of documents?
 A_norm = normalize(full(A));
@@ -24,19 +27,32 @@ k = 50;
 %Cluster, idx contains labels with cluster index, C contains centroids
 [idx, C]= kmeans(A_norm', k);
 C = normalize(C'); idx = idx';
+histogram(idx)
 
 %Compute the thin-QR
-[Q, R] = qr(C,0);
+[P_k, R] = qr(C,0);
 
 %Create G_k
-G_k = Q'*A_norm;
+G_k = P_k'*A_norm;
 
 %Transform query
-q_k = Q'*q(:,9);
+q_k = P_k'*q(:,9);
 
-%Find closest cluster(?) in new vector space
-Match = CosineDist(q_k, G_k)  > tol;
-Docs = find(Match > 0);
+%Compute distances between query and documents
+tol = 0.19;
+Match = CosineDist(q_k, G_k);
 
+%Find the 3 closest documents in new vector space
+[~, Docs] = maxk(Match, 3);
 %% LGK
 
+
+%% Etc
+
+%Find text from query or doc
+
+%Query
+dict(FIND(q(:,9)),:);
+
+%Doc
+dict(FIND(A(:,409)),:)
